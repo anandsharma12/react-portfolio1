2a30610498cf8d39bdbeb45b8cf86b334ede1d26
anandsharma12 <110158189+anandsharma12@users.noreply.github.com>
2024-12-22 22:06:14 +0530

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -5,11 +5,11 @@ const Testimonial = () => {
     <div id='testimonial'>
       <h2>Testimonial</h2>
       <section>
-        <TestimonialCard name={'ajay'}   feedback={
+        <TestimonialCard name={'Ajay'}   feedback={
             "The frontend development for my website was exceptional, delivering a sleek, user-friendly design that exceeded expectations."
           }/>
              <TestimonialCard name={'Saksham'}   feedback={
-            "The frontend development for my website was exceptional, delivering a sleek, user-friendly design that exceeded expectations."
+            "A highly skilled frontend developer with an excellent ability to transform designs into intuitive, responsive web applications. Consistently delivers high-quality work on time.""
           }/>
              <TestimonialCard name={'Sourabh'}   feedback={
             "Thanks Brother, our platform now has an intuitive interface that enhances user experience and engagement."
@@ -21,7 +21,7 @@ const Testimonial = () => {
 
 const TestimonialCard = ({name,feedback})=>(
     <article>
-        <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="user" />
+        <img src= {} alt="user" />
         <h4>{name}</h4>
     <p>{feedback}</p>
     </article>
