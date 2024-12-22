5c8e9f676b94fb4d739c1badd5d3fcf44048b8dc
anandsharma12 <110158189+anandsharma12@users.noreply.github.com>
2024-12-22 22:08:13 +0530

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -21,7 +21,7 @@ const Testimonial = () => {
 
 const TestimonialCard = ({name,feedback})=>(
     <article>
-        <img src= {} alt="user" />
+        <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="user" />
         <h4>{name}</h4>
     <p>{feedback}</p>
     </article>
